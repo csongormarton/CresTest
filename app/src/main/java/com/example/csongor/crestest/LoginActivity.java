cf3e82d901f7b41b4cdfce88c079f281dfc279e1
Marton Csongor <martonjcs@gmail.com>
2015-12-14 19:00:37 +0200

    Initial commit

---
+++ app/src/main/java/com/example/csongor/crestest/LoginActivity.java
@@ -0,0 +1,30 @@
+package com.example.csongor.crestest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.List;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+//        User user = new User(1, "Arni", "valami2@valami.hu", "123456", false);
+//        Log.e("User", user.toString());
+//        user.save();
+
+        List<User> users = User.listAll(User.class);
+        for(int i=0; i<users.size(); i++){
+            Log.e("User", users.get(i).toString());
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+}
---
+++ app/src/main/java/com/example/csongor/crestest/User.java
@@ -0,0 +1,74 @@
+package com.example.csongor.crestest;
+
+
+import com.orm.SugarRecord;
+
+public class User extends SugarRecord<User> {
+    int userID;
+    String name;
+    String email;
+    String password;
+    boolean admin;
+
+    public User() {
+    }
+
+    public User(int userID, String name, String email, String password, boolean admin) {
+        this.userID = userID;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.admin = admin;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(boolean admin) {
+        this.admin = admin;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userID=" + userID +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", admin=" + admin +
+                '}';
+    }
+}
